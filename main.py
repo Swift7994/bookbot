859a1aeb032a193c22698ea3a992073c8c3ffb2f
Swift7994 <JSwift7994@googlemail.com>
2024-11-28 09:13:21 +0000

    added word and character count

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 def main():
     book_path = get_book_path()
     text = get_book_text(book_path)
+    word_count = get_word_count(text)
+    character_count = get_character_count(text)
     print(text)
+    print(f"Word count: {word_count}")
+    print(f"Character count: {character_count}")
 
 def get_book_path():
     path_input = input("Enter file location: ")
@@ -11,5 +15,20 @@ def get_book_path():
 def get_book_text(book_path):
     with open(book_path) as f:
         return f.read()
+
+def get_word_count(text):
+    words = text.split()
+    return len(words)
+
+def get_character_count(text):
+    character_dict = {}
+    lower_case_text = text.lower()
+    for character in lower_case_text:
+        if character in character_dict:
+            character_dict[character] += 1
+        else:
+            character_dict[character] = 1
+    alphabetical_dict = {key: character_dict[key] for key in sorted(character_dict)}
+    return alphabetical_dict
 
 main()
